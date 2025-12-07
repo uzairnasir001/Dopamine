d3111570e74f61af5e9af62b24ef5a9679ef4bd9
Uzair Nasir <146729230+uzairnasir001@users.noreply.github.com>
2025-12-07 19:59:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,283 @@
+// faq-1
+
+document.querySelectorAll('.faq-1').forEach(faq => {
+
+  const plusBtn = faq.querySelector('.plus');
+  const minusBtn = faq.querySelector('.minus');
+  const dropDown = faq.querySelector('.drop-down');
+
+  // Hide minus button & dropdown initially
+  minusBtn.style.display = "none";
+  dropDown.style.display = "none";
+
+  // Open dropdown
+  plusBtn.addEventListener('click', () => {
+    dropDown.style.display = "block";
+    plusBtn.style.display = "none";
+    minusBtn.style.display = "inline-block";
+  });
+
+  // Close dropdown
+  minusBtn.addEventListener('click', () => {
+    dropDown.style.display = "none";
+    minusBtn.style.display = "none";
+    plusBtn.style.display = "inline-block";
+  });
+
+});
+
+// faq-2
+
+document.querySelectorAll('.faq-2').forEach(faq => {
+
+  const plusBtn = faq.querySelector('.plus');
+  const minusBtn = faq.querySelector('.minus');
+  const dropDown = faq.querySelector('.drop-down');
+
+  // Hide minus button & dropdown initially
+  minusBtn.style.display = "none";
+  dropDown.style.display = "none";
+
+  // Open dropdown
+  plusBtn.addEventListener('click', () => {
+    dropDown.style.display = "block";
+    plusBtn.style.display = "none";
+    minusBtn.style.display = "inline-block";
+  });
+
+  // Close dropdown
+  minusBtn.addEventListener('click', () => {
+    dropDown.style.display = "none";
+    minusBtn.style.display = "none";
+    plusBtn.style.display = "inline-block";
+  });
+
+});
+
+// Faq-3
+
+document.querySelectorAll('.faq-3').forEach(faq => {
+
+  const plusBtn = faq.querySelector('.plus');
+  const minusBtn = faq.querySelector('.minus');
+  const dropDown = faq.querySelector('.drop-down');
+
+  // Hide minus button & dropdown initially
+  minusBtn.style.display = "none";
+  dropDown.style.display = "none";
+
+  // Open dropdown
+  plusBtn.addEventListener('click', () => {
+    dropDown.style.display = "block";
+    plusBtn.style.display = "none";
+    minusBtn.style.display = "inline-block";
+  });
+
+  // Close dropdown
+  minusBtn.addEventListener('click', () => {
+    dropDown.style.display = "none";
+    minusBtn.style.display = "none";
+    plusBtn.style.display = "inline-block";
+  });
+
+});
+
+
+// faq-4
+
+
+document.querySelectorAll('.faq-4').forEach(faq => {
+
+  const plusBtn = faq.querySelector('.plus');
+  const minusBtn = faq.querySelector('.minus');
+  const dropDown = faq.querySelector('.drop-down');
+
+  // Hide minus button & dropdown initially
+  minusBtn.style.display = "none";
+  dropDown.style.display = "none";
+
+  // Open dropdown
+  plusBtn.addEventListener('click', () => {
+    dropDown.style.display = "block";
+    plusBtn.style.display = "none";
+    minusBtn.style.display = "inline-block";
+  });
+
+  // Close dropdown
+  minusBtn.addEventListener('click', () => {
+    dropDown.style.display = "none";
+    minusBtn.style.display = "none";
+    plusBtn.style.display = "inline-block";
+  });
+
+});
+
+// faq-5
+
+
+document.querySelectorAll('.faq-5').forEach(faq => {
+
+  const plusBtn = faq.querySelector('.plus');
+  const minusBtn = faq.querySelector('.minus');
+  const dropDown = faq.querySelector('.drop-down');
+
+  // Hide minus button & dropdown initially
+  minusBtn.style.display = "none";
+  dropDown.style.display = "none";
+
+  // Open dropdown
+  plusBtn.addEventListener('click', () => {
+    dropDown.style.display = "block";
+    plusBtn.style.display = "none";
+    minusBtn.style.display = "inline-block";
+  });
+
+  // Close dropdown
+  minusBtn.addEventListener('click', () => {
+    dropDown.style.display = "none";
+    minusBtn.style.display = "none";
+    plusBtn.style.display = "inline-block";
+  });
+
+});
+
+
+// faq-6
+
+document.querySelectorAll('.faq-6').forEach(faq => {
+
+  const plusBtn = faq.querySelector('.plus');
+  const minusBtn = faq.querySelector('.minus');
+  const dropDown = faq.querySelector('.drop-down');
+
+  // Hide minus button & dropdown initially
+  minusBtn.style.display = "none";
+  dropDown.style.display = "none";
+
+  // Open dropdown
+  plusBtn.addEventListener('click', () => {
+    dropDown.style.display = "block";
+    plusBtn.style.display = "none";
+    minusBtn.style.display = "inline-block";
+  });
+
+  // Close dropdown
+  minusBtn.addEventListener('click', () => {
+    dropDown.style.display = "none";
+    minusBtn.style.display = "none";
+    plusBtn.style.display = "inline-block";
+  });
+
+});
+
+// faq-7
+
+document.querySelectorAll('.faq-7').forEach(faq => {
+
+  const plusBtn = faq.querySelector('.plus');
+  const minusBtn = faq.querySelector('.minus');
+  const dropDown = faq.querySelector('.drop-down');
+
+  // Hide minus button & dropdown initially
+  minusBtn.style.display = "none";
+  dropDown.style.display = "none";
+
+  // Open dropdown
+  plusBtn.addEventListener('click', () => {
+    dropDown.style.display = "block";
+    plusBtn.style.display = "none";
+    minusBtn.style.display = "inline-block";
+  });
+
+  // Close dropdown
+  minusBtn.addEventListener('click', () => {
+    dropDown.style.display = "none";
+    minusBtn.style.display = "none";
+    plusBtn.style.display = "inline-block";
+  });
+
+});
+
+// faq-8
+
+document.querySelectorAll('.faq-8').forEach(faq => {
+
+  const plusBtn = faq.querySelector('.plus');
+  const minusBtn = faq.querySelector('.minus');
+  const dropDown = faq.querySelector('.drop-down');
+
+  // Hide minus button & dropdown initially
+  minusBtn.style.display = "none";
+  dropDown.style.display = "none";
+
+  // Open dropdown
+  plusBtn.addEventListener('click', () => {
+    dropDown.style.display = "block";
+    plusBtn.style.display = "none";
+    minusBtn.style.display = "inline-block";
+  });
+
+  // Close dropdown
+  minusBtn.addEventListener('click', () => {
+    dropDown.style.display = "none";
+    minusBtn.style.display = "none";
+    plusBtn.style.display = "inline-block";
+  });
+
+});
+
+// faq-9
+
+document.querySelectorAll('.faq-9').forEach(faq => {
+
+  const plusBtn = faq.querySelector('.plus');
+  const minusBtn = faq.querySelector('.minus');
+  const dropDown = faq.querySelector('.drop-down');
+
+  // Hide minus button & dropdown initially
+  minusBtn.style.display = "none";
+  dropDown.style.display = "none";
+
+  // Open dropdown
+  plusBtn.addEventListener('click', () => {
+    dropDown.style.display = "block";
+    plusBtn.style.display = "none";
+    minusBtn.style.display = "inline-block";
+  });
+
+  // Close dropdown
+  minusBtn.addEventListener('click', () => {
+    dropDown.style.display = "none";
+    minusBtn.style.display = "none";
+    plusBtn.style.display = "inline-block";
+  });
+
+});
+
+// faq-10
+
+document.querySelectorAll('.faq-10').forEach(faq => {
+
+  const plusBtn = faq.querySelector('.plus');
+  const minusBtn = faq.querySelector('.minus');
+  const dropDown = faq.querySelector('.drop-down');
+
+  // Hide minus button & dropdown initially
+  minusBtn.style.display = "none";
+  dropDown.style.display = "none";
+
+  // Open dropdown
+  plusBtn.addEventListener('click', () => {
+    dropDown.style.display = "block";
+    plusBtn.style.display = "none";
+    minusBtn.style.display = "inline-block";
+  });
+
+  // Close dropdown
+  minusBtn.addEventListener('click', () => {
+    dropDown.style.display = "none";
+    minusBtn.style.display = "none";
+    plusBtn.style.display = "inline-block";
+  });
+
+});
